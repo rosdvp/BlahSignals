c8d232e2706ef8d1e1daaaea976e211ee2fa4f2a
rosdvp <45929573+rosdvp@users.noreply.github.com>
2023-02-02 21:46:33 +0300

    BlahOrderedSystemsExampleContainer. Comments

--- AutoOrder/Runtime/BlahOrderedSystemsExampleContainer.cs
+++ AutoOrder/Runtime/BlahOrderedSystemsExampleContainer.cs
@@ -3,7 +3,7 @@ namespace BlahSignals.AutoOrder
 /// <summary>
 /// 1. Copy that file to any folder of your project.<br/>
 /// 2. Change the class name and file name to EcsOrderedSystemsContainer (without Example).<br/>
-/// 3. Uncomment AddOrderedSystems method and don't forget about usings.<br/>
+/// 3. Uncomment AddOrderedSystems method and modify parameter type.<br/>
 /// 3. In your EcsStartup (or what you have to setup systems) call AddOrderedSystems
 /// method to Add systems in a correct order.<br/>
 /// 4. Follow instructions in <see cref="BlahUnOrderedSystemsExampleContainer"/>.
